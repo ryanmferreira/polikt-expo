0103c5fba279f91e4990eea80e645530ade1e6b8
Murilo Rossi <murilorossideandrade@gmail.com>
2026-08-20 23:20:06 -0300

    feat: add guide and news content screens.

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import Ionicons from '@react-native-vector-icons/ionicons';
+import  Ionicons  from '@react-native-vector-icons/ionicons';
 import { Tabs } from 'expo-router';
 import { BORDER_RADIUS, BORDER_WIDTH, COLORS, SPACING } from '../../constants/theme';
 
